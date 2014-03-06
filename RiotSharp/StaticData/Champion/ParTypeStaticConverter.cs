8ed75dadefa27529ad4d52ddfc0a02bdabdd7f02
BenjaminFradet <benjamin.fradet@gmail.com>
2014-03-07 00:36:35 +0100

    fix for reserializing in the static api

--- RiotSharp/StaticData/Champion/ParTypeStaticConverter.cs
+++ RiotSharp/StaticData/Champion/ParTypeStaticConverter.cs
@@ -17,35 +17,39 @@ namespace RiotSharp
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            var token = JToken.Load(reader);
-            var str = token.Value<string>();
-            switch (str)
+            if (reader.Value != null)
             {
-                case "BloodWell":
-                    return ParTypeStatic.BloodWell;
-                case "Mana":
-                    return ParTypeStatic.Mana;
-                case "Energy":
-                    return ParTypeStatic.Energy;
-                case "None":
-                    return ParTypeStatic.None;
-                case "Shield":
-                    return ParTypeStatic.Shield;
-                case "Rage":
-                    return ParTypeStatic.Rage;
-                case "Ferocity":
-                    return ParTypeStatic.Ferocity;
-                case "Heat":
-                    return ParTypeStatic.Heat;
-                case "Dragonfury":
-                    return ParTypeStatic.Dragonfury;
-                case "Battlefury":
-                    return ParTypeStatic.Battlefury;
-                case "Wind":
-                    return ParTypeStatic.Wind;
-                default:
-                    return null;
+                var token = JToken.Load(reader);
+                var str = token.Value<string>();
+                switch (str)
+                {
+                    case "BloodWell":
+                        return ParTypeStatic.BloodWell;
+                    case "Mana":
+                        return ParTypeStatic.Mana;
+                    case "Energy":
+                        return ParTypeStatic.Energy;
+                    case "None":
+                        return ParTypeStatic.None;
+                    case "Shield":
+                        return ParTypeStatic.Shield;
+                    case "Rage":
+                        return ParTypeStatic.Rage;
+                    case "Ferocity":
+                        return ParTypeStatic.Ferocity;
+                    case "Heat":
+                        return ParTypeStatic.Heat;
+                    case "Dragonfury":
+                        return ParTypeStatic.Dragonfury;
+                    case "Battlefury":
+                        return ParTypeStatic.Battlefury;
+                    case "Wind":
+                        return ParTypeStatic.Wind;
+                    default:
+                        return null;
+                }
             }
+            return null;
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
