bf916d160ac81a15f8eee70e6d861c10c2c295e9
Bert Goens <bertgoens@gmail.com>
2014-06-02 19:35:44 +0200

    StaticData\Champion XML edits
    Removed unused usings;
    Added more xml comments.

--- RiotSharp/StaticData/Champion/ParTypeStatic.cs
+++ RiotSharp/StaticData/Champion/ParTypeStatic.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RiotSharp
+﻿namespace RiotSharp
 {
     /// <summary>
     /// Enum representing a champion's resource.
