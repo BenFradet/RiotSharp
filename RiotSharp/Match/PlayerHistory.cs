27f17992b4d952f559059b3fe0699dff3b22a914
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-09-09 16:03:47 +0200

    added classes for the match history endpoint #62

---
+++ RiotSharp/Match/PlayerHistory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace RiotSharp
+{
+    /// <summary>
+    /// Match history of a player (Match API).
+    /// </summary>
+    public class PlayerHistory
+    {
+        internal PlayerHistory() { }
+
+        /// <summary>
+        /// List of matches for the player.
+        /// </summary>
+        [JsonProperty("matches")]
+        public List<MatchSummary> Matches { get; set; }
+    }
+}
