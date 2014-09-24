9c52d1cd1740fc3e06fafbef34ef963840787ff5
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-09-24 17:58:53 +0200

    removed unused using in test classes

--- RiotSharpTest/RiotApiTest.cs
+++ RiotSharpTest/RiotApiTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
--- RiotSharpTest/StaticRiotApiExceptionTest.cs
+++ RiotSharpTest/StaticRiotApiExceptionTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Configuration;
 
 using RiotSharp;
