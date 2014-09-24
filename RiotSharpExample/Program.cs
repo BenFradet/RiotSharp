db352168e7959c31279c4486af8def6048f9f770
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-09-24 18:44:23 +0200

    update to the example project

--- RiotSharpExample/Program.cs
+++ RiotSharpExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Globalization;
 using RiotSharp;
 
 namespace RiotSharpExample
@@ -10,12 +11,17 @@ namespace RiotSharpExample
         {
             var api = RiotApi.GetInstance(ConfigurationManager.AppSettings["ApiKey"]);
             var staticApi = StaticRiotApi.GetInstance(ConfigurationManager.AppSettings["ApiKey"]);
+            var statusApi = StatusRiotApi.GetInstance();
             int id = int.Parse(ConfigurationManager.AppSettings["Summoner1Id"]);
             string name = ConfigurationManager.AppSettings["Summoner1Name"];
             int id2 = int.Parse(ConfigurationManager.AppSettings["Summoner2Id"]);
             string name2 = ConfigurationManager.AppSettings["Summoner2Name"];
             string team = ConfigurationManager.AppSettings["Team1Id"];
             string team2 = ConfigurationManager.AppSettings["Team2Id"];
+
+            var shards = statusApi.GetShards();
+
+            var shardStatus = statusApi.GetShardStatus(Region.euw);
 
             var games = api.GetRecentGames(Region.euw, id);
 
