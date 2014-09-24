50f309272d19b0ced421ec5f04a043b479ed55a2
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-09-24 17:48:23 +0200

    added classes for the status endpoint #68

---
+++ RiotSharp/Misc/DateTimeConverterFromString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RiotSharp
+{
+    class DateTimeConverterFromString : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(string).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            var token = JToken.Load(reader);
+            if (token.Value<string>() != null)
+            {
+                var str = token.Value<string>();
+                return DateTime.Parse(str);
+            }
+            return null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, ((DateTime)value).ToString("yyyy-MM-ddTHH:mm:ss.zzzZ"));
+        }
+    }
+}
