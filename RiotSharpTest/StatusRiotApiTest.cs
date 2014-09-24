9839787f43c4afbfb81844289770f727f0680c2d
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-09-24 18:46:55 +0200

    removed unused using in StatusRiotApiTest, fixes #68

--- RiotSharpTest/StatusRiotApiTest.cs
+++ RiotSharpTest/StatusRiotApiTest.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using RiotSharp;
-using RiotSharp.StatusEndpoint;
 
 namespace RiotSharpTest
 {
