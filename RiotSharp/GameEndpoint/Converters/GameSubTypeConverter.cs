1209c1f439de73c6ac90701a121bf6d9421a5730
Jeffroiscool <Jeffroiscool@gmail.com>
2014-10-28 09:27:59 +0100

    Changed SR_6x6 to HEXAKILL
    If this is not implemented GetRecentGames will crash for people who have played the new Hexakill lately.

--- RiotSharp/GameEndpoint/Converters/GameSubTypeConverter.cs
+++ RiotSharp/GameEndpoint/Converters/GameSubTypeConverter.cs
@@ -50,7 +50,7 @@ namespace RiotSharp.GameEndpoint
                         return GameSubType.FirstBlood1x1;
                     case "FIRSTBLOOD_2x2":
                         return GameSubType.FirstBlood2x2;
-                    case "SR_6x6":
+                    case "HEXAKILL":
                         return GameSubType.Hexakill;
                     case "CAP_5x5":
                         return GameSubType.TeamBuilder5x5;
