08f4d8de701e087e4d40feab45e1646867cb537e
Benjamin Fradet <benjamin.fradet@gmail.com>
2014-11-21 15:24:50 +0100

    few changes in Program.cs

--- RiotSharpExample/Program.cs
+++ RiotSharpExample/Program.cs
@@ -21,11 +21,25 @@ namespace RiotSharpExample
             string team = ConfigurationManager.AppSettings["Team1Id"];
             string team2 = ConfigurationManager.AppSettings["Team2Id"];
 
+            var summoner = api.GetSummoner(Region.euw, "TommyTrueLove");
+            var stats = summoner.GetStatsSummaries();
+            foreach (var stat in stats)
+            {
+                Console.WriteLine(stat.Wins);
+            }
+
             var shards = statusApi.GetShards();
 
             var shardStatus = statusApi.GetShardStatus(Region.euw);
 
             var statSummaries = api.GetStatsSummaries(Region.euw, id);
+
+            var statsRanked = api.GetStatsRanked(Region.euw, id, RiotSharp.StatsEndpoint.Season.Season4);
+
+            foreach (var stat in statsRanked)
+            {
+                Console.WriteLine(stat.Stats.RankedSoloGamesPlayed);
+            }
 
             var championIds = new List<int>();
             for (int i = 0; i < 30; i += 15)
