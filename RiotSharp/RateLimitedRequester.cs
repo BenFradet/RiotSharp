68b58d928ca70090dbdbeaca61be7007635dee86
Giswald <Christoph.Weigert93@gmail.com>
2014-12-30 00:00:22 +0100

    Rate Limit per Region

--- RiotSharp/RateLimitedRequester.cs
+++ RiotSharp/RateLimitedRequester.cs
@@ -1,13 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="RateLimitedRequester.cs" company="">
-//
-// </copyright>
-// <summary>
-//   The rate limited requester.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading;
@@ -15,222 +6,111 @@ using System.Threading.Tasks;
 
 namespace RiotSharp
 {
-    /// <summary>
-    /// The rate limited requester.
-    /// </summary>
     class RateLimitedRequester : Requester
     {
-        /// <summary>
-        /// The instance.
-        /// </summary>
         private static RateLimitedRequester instance;
-
-        /// <summary>
-        /// Prevents a default instance of the <see cref="RateLimitedRequester"/> class from being created.
-        /// </summary>
-        private RateLimitedRequester()
-        {
-        }
-
-        /// <summary>
-        /// Gets the instance.
-        /// </summary>
+        private RateLimitedRequester() { }
         public static new RateLimitedRequester Instance
         {
-            get
-            {
-                return instance ?? (instance = new RateLimitedRequester());
-            }
+            get { return instance ?? (instance = new RateLimitedRequester()); }
         }
 
-        /// <summary>
-        /// Gets or sets the rate limit per 10 s.
-        /// </summary>
         public static int RateLimitPer10S { get; set; }
-
-        /// <summary>
-        /// Gets or sets the rate limit per 10 m.
-        /// </summary>
         public static int RateLimitPer10M { get; set; }
 
-        /// <summary>
-        /// The first request in last ten s.
-        /// </summary>
-        private DateTime firstRequestInLastTenS = DateTime.MinValue;
+        //private DateTime firstRequestInLastTenS = DateTime.MinValue;
+        //private DateTime firstRequestInLastTenM = DateTime.MinValue;
+        //private int numberOfRequestsInLastTenS;
+        //private int numberOfRequestInLastTenM;
 
-        /// <summary>
-        /// The first request in last ten m.
-        /// </summary>
-        private DateTime firstRequestInLastTenM = DateTime.MinValue;
+        private Dictionary<Region, DateTime> firstRequestInLastTenS = new Dictionary<Region, DateTime>();
+        private Dictionary<Region, DateTime> firstRequestInLastTenM = new Dictionary<Region, DateTime>();
+        private Dictionary<Region, int> numberOfRequestsInLastTenS = new Dictionary<Region, int>();
+        private Dictionary<Region, int> numberOfRequestInLastTenM = new Dictionary<Region, int>();
 
-        /// <summary>
-        /// The number of requests in last ten s.
-        /// </summary>
-        private int numberOfRequestsInLastTenS;
+        private SemaphoreSlim semaphore = new SemaphoreSlim(1);
 
-        /// <summary>
-        /// The number of request in last ten m.
-        /// </summary>
-        private int numberOfRequestInLastTenM;
-
-        /// <summary>
-        /// The semaphore.
-        /// </summary>
-        private readonly SemaphoreSlim semaphore = new SemaphoreSlim(1);
-
-        /// <summary>
-        /// The create request.
-        /// </summary>
-        /// <param name="relativeUrl">
-        /// The relative url.
-        /// </param>
-        /// <param name="region">
-        /// The region.
-        /// </param>
-        /// <param name="addedArguments">
-        /// The added arguments.
-        /// </param>
-        /// <param name="useSsl">
-        /// The use Ssl.
-        /// </param>
-        /// <returns>
-        /// The <see cref="string"/>.
-        /// </returns>
-        public string CreateRequest(string relativeUrl, Region region, List<string> addedArguments = null, bool useSsl = true)
+        public async Task<string> CreateRequestAsync(string relativeUrl, Region region,
+            List<string> addedArguments = null)
         {
             rootDomain = region + ".api.pvp.net";
-            HttpWebRequest request = PrepareRequest(relativeUrl, addedArguments, useSsl);
-
-            semaphore.Wait();
-            {
-                if (firstRequestInLastTenM == DateTime.MinValue)
-                {
-                    firstRequestInLastTenM = DateTime.Now;
-                }
-
-                numberOfRequestInLastTenM++;
-
-                if (firstRequestInLastTenS == DateTime.MinValue)
-                {
-                    firstRequestInLastTenS = DateTime.Now;
-                }
-
-                numberOfRequestsInLastTenS++;
-
-                if (numberOfRequestInLastTenM > RateLimitPer10M)
-                {
-                    while ((DateTime.Now - firstRequestInLastTenM).TotalSeconds <= 601)
-                    {
-                    }
-
-                    numberOfRequestInLastTenM = 1;
-                    firstRequestInLastTenM = DateTime.Now;
-                }
-
-                if (numberOfRequestsInLastTenS > RateLimitPer10S)
-                {
-                    while ((DateTime.Now - firstRequestInLastTenS).TotalSeconds <= 11)
-                    {
-                    }
-
-                    numberOfRequestsInLastTenS = 1;
-                    firstRequestInLastTenS = DateTime.Now;
-                }
-
-                if ((DateTime.Now - firstRequestInLastTenS).TotalSeconds > 10)
-                {
-                    firstRequestInLastTenS = DateTime.Now;
-                    numberOfRequestsInLastTenS = 1;
-                }
-
-                if ((DateTime.Now - firstRequestInLastTenM).TotalMinutes > 10)
-                {
-                    firstRequestInLastTenM = DateTime.Now;
-                    numberOfRequestInLastTenM = 1;
-                }
-            }
-
-            semaphore.Release();
-
-            return GetResponse(request);
-        }
-
-        /// <summary>
-        /// The create request async.
-        /// </summary>
-        /// <param name="relativeUrl">
-        /// The relative url.
-        /// </param>
-        /// <param name="region">
-        /// The region.
-        /// </param>
-        /// <param name="addedArguments">
-        /// The added arguments.
-        /// </param>
-        /// <param name="useSsl">
-        /// The use Ssl.
-        /// </param>
-        /// <returns>
-        /// The <see cref="Task"/>.
-        /// </returns>
-        public async Task<string> CreateRequestAsync(
-            string relativeUrl,
-            Region region,
-            List<string> addedArguments = null,
-            bool useSsl = true)
-        {
-            rootDomain = region + ".api.pvp.net";
-            HttpWebRequest request = PrepareRequest(relativeUrl, addedArguments, useSsl);
+            HttpWebRequest request = PrepareRequest(relativeUrl, addedArguments);
 
             await semaphore.WaitAsync();
             {
-                if (firstRequestInLastTenM == DateTime.MinValue)
+                if (firstRequestInLastTenM.ContainsKey(region))
                 {
-                    firstRequestInLastTenM = DateTime.Now;
+                    if (firstRequestInLastTenM[region] == DateTime.MinValue)
+                    {
+                        firstRequestInLastTenM[region] = DateTime.Now;
+                    }
+                }
+                else
+                {
+                    firstRequestInLastTenM.Add(region, DateTime.MinValue);
                 }
 
-                numberOfRequestInLastTenM++;
-
-                if (firstRequestInLastTenS == DateTime.MinValue)
+                if (numberOfRequestInLastTenM.ContainsKey(region))
                 {
-                    firstRequestInLastTenS = DateTime.Now;
+                    numberOfRequestInLastTenM[region] = numberOfRequestInLastTenM[region]++;
+                }
+                else
+                {
+                    numberOfRequestInLastTenM.Add(region, 1);
                 }
 
-                numberOfRequestsInLastTenS++;
 
-                if (numberOfRequestInLastTenM > RateLimitPer10M)
+                if (firstRequestInLastTenS.ContainsKey(region))
                 {
-                    while ((DateTime.Now - firstRequestInLastTenM).TotalSeconds <= 601)
+                    if (firstRequestInLastTenS[region] == DateTime.MinValue)
+                    {
+                        firstRequestInLastTenS[region] = DateTime.Now;
+                    }
+                }
+                else
+                {
+                    firstRequestInLastTenS.Add(region, DateTime.MinValue);
+                }
+
+                if (numberOfRequestsInLastTenS.ContainsKey(region))
+                {
+                    numberOfRequestsInLastTenS[region] = numberOfRequestsInLastTenS[region]++;
+                }
+                else
+                {
+                    numberOfRequestsInLastTenS.Add(region, 1);
+                }
+
+
+                if (numberOfRequestInLastTenM[region] > RateLimitPer10M)
+                {
+                    while ((DateTime.Now - firstRequestInLastTenM[region]).TotalSeconds <= 601)
                     {
                     }
-
-                    numberOfRequestInLastTenM = 1;
-                    firstRequestInLastTenM = DateTime.Now;
+                    numberOfRequestInLastTenM[region] = 1;
+                    firstRequestInLastTenM[region] = DateTime.Now;
                 }
-
-                if (numberOfRequestsInLastTenS > RateLimitPer10S)
+                if (numberOfRequestsInLastTenS[region] > RateLimitPer10S)
                 {
-                    while ((DateTime.Now - firstRequestInLastTenS).TotalSeconds <= 11)
+                    while ((DateTime.Now - firstRequestInLastTenS[region]).TotalSeconds <= 11)
                     {
                     }
-
-                    numberOfRequestsInLastTenS = 1;
-                    firstRequestInLastTenS = DateTime.Now;
+                    numberOfRequestsInLastTenS[region] = 1;
+                    firstRequestInLastTenS[region] = DateTime.Now;
                 }
 
-                if ((DateTime.Now - firstRequestInLastTenS).TotalSeconds > 10)
+                if ((DateTime.Now - firstRequestInLastTenS[region]).TotalSeconds > 10)
                 {
-                    firstRequestInLastTenS = DateTime.Now;
-                    numberOfRequestsInLastTenS = 1;
+                    firstRequestInLastTenS[region] = DateTime.Now;
+                    numberOfRequestsInLastTenS[region] = 1;
+                }
+                if ((DateTime.Now - firstRequestInLastTenM[region]).TotalMinutes > 10)
+                {
+                    firstRequestInLastTenM[region] = DateTime.Now;
+                    numberOfRequestInLastTenM[region] = 1;
                 }
 
-                if ((DateTime.Now - firstRequestInLastTenM).TotalMinutes > 10)
-                {
-                    firstRequestInLastTenM = DateTime.Now;
-                    numberOfRequestInLastTenM = 1;
-                }
             }
-
             semaphore.Release();
 
             return await GetResponseAsync(request);
