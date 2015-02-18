1e5722d3f8f408ab45f018c69bff703a5c5ebc91
Benjamin Fradet <benjamin.fradet@gmail.com>
2015-02-18 22:57:23 +0100

    started working on the language strings endpoint, #96

---
+++ RiotSharp/StaticDataEndpoint/LanguageStringsData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace RiotSharp.StaticDataEndpoint
+{
+    /// <summary>
+    /// Class representing data returned by the language strings endpoint (Static API).
+    /// </summary>
+    [Serializable]
+    public class LanguageStringsData
+    {
+        internal LanguageStringsData() { }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("version")]
+        public string Version { get; set; }
+
+        [JsonProperty("data")]
+        public Dictionary<String, String> Data { get; set; }
+    }
+}
