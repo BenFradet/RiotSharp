a52d0711df889d5d8a9da8c7371f1ea6a69ebcfc
Benjamin Fradet <benjamin.fradet@gmail.com>
2015-02-21 20:42:10 +0100

    2.2.* in AssemblyInfo

--- RiotSharp/Properties/AssemblyInfo.cs
+++ RiotSharp/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BenFradet")]
 [assembly: AssemblyProduct("RiotSharp")]
-[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyCopyright("Copyright (c) Benjamin Fradet 2013-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("2.2.*")]
 //[assembly: AssemblyFileVersion("1.0.*")]
