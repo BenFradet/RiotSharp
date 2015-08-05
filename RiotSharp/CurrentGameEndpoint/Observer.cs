cbd7aea008348eecdfe4c7ac7df03625d7a8b363
aktai0 <xtoken.x@gmail.com>
2015-08-05 00:10:34 -0700

    Changed Tabs -> Space Indentation in Source Files

--- RiotSharp/CurrentGameEndpoint/Observer.cs
+++ RiotSharp/CurrentGameEndpoint/Observer.cs
@@ -2,15 +2,15 @@
 
 namespace RiotSharp.CurrentGameEndpoint
 {
-	/// <summary>
-	/// Class representing an Observer in the API.
-	/// </summary>
-	public class Observer
-	{
-		/// <summary>
-		/// Key required to pass to the LoL client to spectate a game.
-		/// </summary>
-		[JsonProperty("encryptionKey")]
-		public string EncryptionKey { get; set; }
-	}
+    /// <summary>
+    /// Class representing an Observer in the API.
+    /// </summary>
+    public class Observer
+    {
+        /// <summary>
+        /// Key required to pass to the LoL client to spectate a game.
+        /// </summary>
+        [JsonProperty("encryptionKey")]
+        public string EncryptionKey { get; set; }
+    }
 }
