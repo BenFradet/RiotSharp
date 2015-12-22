17f5596170e053571645aa721f7f1607afa008c4
Joris <jorisvanvugt@live.nl>
2015-12-22 16:15:36 +0100

    Remove unused using statements

--- RiotSharp/TournamentEndpoint/TournamentProvider.cs
+++ RiotSharp/TournamentEndpoint/TournamentProvider.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RiotSharp.TournamentEndpoint
--- RiotSharp/TournamentEndpoint/TournamentSpectatorType.cs
+++ RiotSharp/TournamentEndpoint/TournamentSpectatorType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RiotSharp.TournamentEndpoint
+﻿namespace RiotSharp.TournamentEndpoint
 {
     /// <summary>
     /// Spectator type of the game (Tournament API).
