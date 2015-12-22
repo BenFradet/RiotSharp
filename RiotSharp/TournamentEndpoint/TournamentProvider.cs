3fb84323350c101476927b41b8d9b2ddc8a2e89d
Steven Reitsma <steven@properchaos.nl>
2015-12-22 20:42:23 +0100

    Documentation.

--- RiotSharp/TournamentEndpoint/TournamentProvider.cs
+++ RiotSharp/TournamentEndpoint/TournamentProvider.cs
@@ -10,11 +10,21 @@ namespace RiotSharp.TournamentEndpoint
 
         public int Id { get; set; }
 
+        /// <summary>
+        /// Creates a tournament for this provider.
+        /// </summary>
+        /// <param name="name">An optional tournament name.</param>
+        /// <returns>A tournament instance.</returns>
         public Tournament CreateTournament(string name)
         {
             return TournamentRiotApi.GetInstance().CreateTournament(Id, name);
         }
 
+        /// <summary>
+        /// Asynchronously creates a tournament for this provider.
+        /// </summary>
+        /// <param name="name">An optional tournament name.</param>
+        /// <returns>A tournament instance.</returns>
         public async Task<Tournament> CreateTournamentAsync(string name)
         {
             return await TournamentRiotApi.GetInstance().CreateTournamentAsync(Id, name);
--- RiotSharp/TournamentRiotApi.cs
+++ RiotSharp/TournamentRiotApi.cs
@@ -60,6 +60,12 @@ namespace RiotSharp
             requester = Requesters.TournamentRequester;
         }
 
+        /// <summary>
+        /// Creates a tournament provider and returns its ID.
+        /// </summary>
+        /// <param name="region">The region in which the provider will be running tournaments.</param>
+        /// <param name="url">The provider's callback URL to which tournament game results in this region should be posted. The URL must be well-formed, use the http or https protocol, and use the default port for the protocol (http URLs must use port 80, https URLs must use port 443).</param>
+        /// <returns></returns>
         public TournamentProvider CreateProvider(Region region, string url)
         {
             var body = new Dictionary<string, object> { { "url", url }, { "region", region.ToString().ToUpper() } };
@@ -71,6 +77,12 @@ namespace RiotSharp
             return obj;
         }
 
+        /// <summary>
+        /// Creates a tournament provider and returns its object.
+        /// </summary>
+        /// <param name="region">The region in which the provider will be running tournaments.</param>
+        /// <param name="url">The provider's callback URL to which tournament game results in this region should be posted. The URL must be well-formed, use the http or https protocol, and use the default port for the protocol (http URLs must use port 80, https URLs must use port 443).</param>
+        /// <returns></returns>
         public async Task<TournamentProvider> CreateProviderAsync(Region region, string url)
         {
             var body = new Dictionary<string, object> { { "url", url }, { "region", region.ToString().ToUpper() } };
@@ -79,6 +91,12 @@ namespace RiotSharp
             return await Task.Factory.StartNew(() => { var t = new TournamentProvider { Id = int.Parse(json) }; Console.WriteLine(json); return t; });
         }
 
+        /// <summary>
+        /// Creates a tournament and returns its object.
+        /// </summary>
+        /// <param name="providerId">The provider ID to specify the regional registered provider data to associate this tournament.</param>
+        /// <param name="name">The optional name of the tournament.</param>
+        /// <returns></returns>
         public Tournament CreateTournament(int providerId, string name)
         {
             var body = new Dictionary<string, object> { { "name", name }, { "providerId", providerId } };
@@ -90,6 +108,12 @@ namespace RiotSharp
             return obj;
         }
 
+        /// <summary>
+        /// Creates a tournament and returns its object.
+        /// </summary>
+        /// <param name="providerId">The provider ID to specify the regional registered provider data to associate this tournament.</param>
+        /// <param name="name">The optional name of the tournament.</param>
+        /// <returns></returns>
         public async Task<Tournament> CreateTournamentAsync(int providerId, string name)
         {
             var body = new Dictionary<string, object> { { "name", name }, { "providerId", providerId } };
@@ -101,6 +125,17 @@ namespace RiotSharp
             return await Task.Factory.StartNew(() => obj);
         }
 
+        /// <summary>
+        /// Create a tournament code for the given tournament id.
+        /// </summary>
+        /// <param name="tournamentId">The tournament ID</param>
+        /// <param name="teamSize">The team size of the game. Valid values are 1-5.</param>
+        /// <param name="allowedSummonerIds">Optional list of participants in order to validate the players eligible to join the lobby. NOTE: We currently do not enforce participants at the team level, but rather the aggregate of teamOne and teamTwo. We may add the ability to enforce at the team level in the future.</param>
+        /// <param name="spectatorType">The spectator type of the game.</param>
+        /// <param name="pickType">The pick type of the game.</param>
+        /// <param name="mapType">The map type of the game.</param>
+        /// <param name="metaData">Optional string that may contain any data in any format, if specified at all. Used to denote any custom information about the game.</param>
+        /// <returns>The tournament code in string format.</returns>
         public string CreateTournamentCode(int tournamentId, int teamSize, List<long> allowedSummonerIds, TournamentSpectatorType spectatorType, TournamentPickType pickType, TournamentMapType mapType, string metaData)
         {
             var body = new Dictionary<string, object> { { "teamSize", teamSize }, { "allowedSummonerIds", new Dictionary<string, object> { { "participants", allowedSummonerIds } } },{ "spectatorType", spectatorType }, { "pickType", pickType }, { "mapType", mapType }, { "metadata", metaData } };
@@ -112,6 +147,17 @@ namespace RiotSharp
             return obj[0];
         }
 
+        /// <summary>
+        /// Create a tournament code for the given tournament id.
+        /// </summary>
+        /// <param name="tournamentId">The tournament ID</param>
+        /// <param name="teamSize">The team size of the game. Valid values are 1-5.</param>
+        /// <param name="allowedSummonerIds">Optional list of participants in order to validate the players eligible to join the lobby. NOTE: We currently do not enforce participants at the team level, but rather the aggregate of teamOne and teamTwo. We may add the ability to enforce at the team level in the future.</param>
+        /// <param name="spectatorType">The spectator type of the game.</param>
+        /// <param name="pickType">The pick type of the game.</param>
+        /// <param name="mapType">The map type of the game.</param>
+        /// <param name="metaData">Optional string that may contain any data in any format, if specified at all. Used to denote any custom information about the game.</param>
+        /// <returns>The tournament code in string format.</returns>
         public async Task<string> CreateTournamentCodeAsync(int tournamentId, int teamSize, List<long> allowedSummonerIds, TournamentSpectatorType spectatorType, TournamentPickType pickType, TournamentMapType mapType, string metaData)
         {
             var body = new Dictionary<string, object> { { "teamSize", teamSize }, { "allowedSummonerIds", new Dictionary<string, object> { { "participants", allowedSummonerIds } } }, { "spectatorType", spectatorType }, { "pickType", pickType }, { "mapType", mapType }, { "metadata", metaData } };
@@ -120,6 +166,17 @@ namespace RiotSharp
             return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<List<string>>(json)[0]);
         }
 
+        /// <summary>
+        /// Create multiple tournament codes for the given tournament id.
+        /// </summary>
+        /// <param name="tournamentId">The tournament ID</param>
+        /// <param name="teamSize">The team size of the game. Valid values are 1-5.</param>
+        /// <param name="spectatorType">The spectator type of the game.</param>
+        /// <param name="pickType">The pick type of the game.</param>
+        /// <param name="mapType">The map type of the game.</param>
+        /// <param name="metaData">Optional string that may contain any data in any format, if specified at all. Used to denote any custom information about the game.</param>
+        /// <param name="count">The number of codes to create (max 1000).</param>
+        /// <returns>A list of tournament codes in string format.</returns>
         public List<string> CreateTournamentCodes(int tournamentId, int teamSize, TournamentSpectatorType spectatorType, TournamentPickType pickType, TournamentMapType mapType, string metaData, int count = 1)
         {
             var body = new Dictionary<string, object> { { "teamSize", teamSize }, { "spectatorType", spectatorType }, { "pickType", pickType }, { "mapType", mapType }, { "metadata", metaData } };
@@ -131,6 +188,17 @@ namespace RiotSharp
             return obj;
         }
 
+        /// <summary>
+        /// Create multiple tournament codes for the given tournament id.
+        /// </summary>
+        /// <param name="tournamentId">The tournament ID</param>
+        /// <param name="teamSize">The team size of the game. Valid values are 1-5.</param>
+        /// <param name="spectatorType">The spectator type of the game.</param>
+        /// <param name="pickType">The pick type of the game.</param>
+        /// <param name="mapType">The map type of the game.</param>
+        /// <param name="metaData">Optional string that may contain any data in any format, if specified at all. Used to denote any custom information about the game.</param>
+        /// <param name="count">The number of codes to create (max 1000).</param>
+        /// <returns>A list of tournament codes in string format.</returns>
         public async Task<List<string>> CreateTournamentCodesAsync(int tournamentId, int teamSize, TournamentSpectatorType spectatorType, TournamentPickType pickType, TournamentMapType mapType, string metaData, int count = 1)
         {
             var body = new Dictionary<string, object> { { "teamSize", teamSize }, { "spectatorType", spectatorType }, { "pickType", pickType }, { "mapType", mapType }, { "metadata", metaData } };
@@ -139,6 +207,11 @@ namespace RiotSharp
             return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<List<string>>(json));
         }
 
+        /// <summary>
+        /// Returns the details of a certain tournament code.
+        /// </summary>
+        /// <param name="tournamentCode">The tournament code in string format.</param>
+        /// <returns>TournamentCodeDetail object with details of the tournament code.</returns>
         public TournamentCodeDetail GetTournamentCodeDetails(string tournamentCode)
         {
             var json = requester.CreateRequest(TournamentRootUrl + string.Format(GetCodeUrl, tournamentCode), Region.global);
@@ -147,6 +220,11 @@ namespace RiotSharp
             return obj;
         }
 
+        /// <summary>
+        /// Returns the details of a certain tournament code.
+        /// </summary>
+        /// <param name="tournamentCode">The tournament code in string format.</param>
+        /// <returns>TournamentCodeDetail object with details of the tournament code.</returns>
         public async Task<TournamentCodeDetail> GetTournamentCodeDetailsAsync(string tournamentCode)
         {
             var json = await requester.CreateRequestAsync(TournamentRootUrl + string.Format(GetCodeUrl, tournamentCode), Region.global);
@@ -154,6 +232,11 @@ namespace RiotSharp
             return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<TournamentCodeDetail>(json));
         }
 
+        /// <summary>
+        /// Gets a list of lobby events by tournament code.
+        /// </summary>
+        /// <param name="tournamentCode">A tournament code in string format.</param>
+        /// <returns>List of TournamentLobbyEvents.</returns>
         public List<TournamentLobbyEvent> GetTournamentLobbyEvents(string tournamentCode)
         {
             var json = requester.CreateRequest(TournamentRootUrl + string.Format(LobbyEventUrl, tournamentCode), Region.global);
@@ -162,6 +245,11 @@ namespace RiotSharp
             return obj["eventList"];
         }
 
+        /// <summary>
+        /// Gets a list of lobby events by tournament code.
+        /// </summary>
+        /// <param name="tournamentCode">A tournament code in string format.</param>
+        /// <returns>List of TournamentLobbyEvents.</returns>
         public async Task<List<TournamentLobbyEvent>> GetTournamentLobbyEventsAsync(string tournamentCode)
         {
             var json = await requester.CreateRequestAsync(TournamentRootUrl + string.Format(LobbyEventUrl, tournamentCode), Region.global);
@@ -169,6 +257,14 @@ namespace RiotSharp
             return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<Dictionary<string, List<TournamentLobbyEvent>>>(json)["eventList"]);
         }
 
+        /// <summary>
+        /// Retrieve match by match ID and tournament code.
+        /// </summary>
+        /// <param name="region">The region of the match.</param>
+        /// <param name="matchId">The ID of the match.</param>
+        /// <param name="tournamentCode">The tournament code of the match.</param>
+        /// <param name="includeTimeline">Flag indicating whether or not to include match timeline data.</param>
+        /// <returns>MatchDetail object.</returns>
         public MatchDetail GetTournamentMatch(Region region, long matchId, string tournamentCode, bool includeTimeline)
         {
             var json = requester.CreateRequest(string.Format(MatchRootUrl, region) + string.Format(GetMatchDetailUrl, matchId), region, new List<string> { string.Format("tournamentCode={0}", tournamentCode), string.Format("includeTimeline={0}", includeTimeline) });
@@ -178,6 +274,14 @@ namespace RiotSharp
             return obj;
         }
 
+        /// <summary>
+        /// Retrieve match by match ID and tournament code.
+        /// </summary>
+        /// <param name="region">The region of the match.</param>
+        /// <param name="matchId">The ID of the match.</param>
+        /// <param name="tournamentCode">The tournament code of the match.</param>
+        /// <param name="includeTimeline">Flag indicating whether or not to include match timeline data.</param>
+        /// <returns>MatchDetail object.</returns>
         public async Task<MatchDetail> GetTournamentMatchAsync(Region region, long matchId, string tournamentCode, bool includeTimeline)
         {
             var json = await requester.CreateRequestAsync(string.Format(MatchRootUrl, region) + string.Format(GetMatchDetailUrl, matchId), region, new List<string> { string.Format("tournamentCode={0}", tournamentCode), string.Format("includeTimeline={0}", includeTimeline) });
@@ -185,6 +289,12 @@ namespace RiotSharp
             return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<MatchDetail>(json));
         }
 
+        /// <summary>
+        /// Retrieve match IDs by tournament code.
+        /// </summary>
+        /// <param name="region">The region of the match.</param>
+        /// <param name="tournamentCode">The tournament code of the match.</param>
+        /// <returns>The match id of the match played with the tournament code entered.</returns>
         public long GetTournamentMatchId(Region region, string tournamentCode)
         {
             var json = requester.CreateRequest(string.Format(MatchRootUrl, region) + string.Format(GetMatchIdUrl, tournamentCode), region);
@@ -194,6 +304,12 @@ namespace RiotSharp
             return obj[0];
         }
 
+        /// <summary>
+        /// Retrieve match IDs by tournament code.
+        /// </summary>
+        /// <param name="region">The region of the match.</param>
+        /// <param name="tournamentCode">The tournament code of the match.</param>
+        /// <returns>The match id of the match played with the tournament code entered.</returns>
         public async Task<long> GetTournamentMatchIdAsync(Region region, string tournamentCode)
         {
             var json = await requester.CreateRequestAsync(string.Format(MatchRootUrl, region) + string.Format(GetMatchIdUrl, tournamentCode), region);
@@ -201,6 +317,14 @@ namespace RiotSharp
             return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<List<long>>(json)[0]);
         }
 
+        /// <summary>
+        /// Update the pick type, map, spectator type, or allowed summoners for a code.
+        /// </summary>
+        /// <param name="tournamentCode">The tournament code to update</param>
+        /// <param name="allowedSummonerIds">List of summoner id's.</param>
+        /// <param name="spectatorType">The spectator type.</param>
+        /// <param name="pickType">The pick type.</param>
+        /// <param name="mapType">The map type.</param>
         public void UpdateTournamentCode(string tournamentCode, List<long> allowedSummonerIds, TournamentSpectatorType? spectatorType, TournamentPickType? pickType, TournamentMapType? mapType)
         {
             var body = BuildTournamentUpdateBody(allowedSummonerIds, spectatorType, pickType, mapType);
@@ -208,6 +332,14 @@ namespace RiotSharp
             requester.CreatePutRequest(TournamentRootUrl + string.Format(PutCodeUrl, tournamentCode), Region.global, JsonConvert.SerializeObject(body));
         }
 
+        /// <summary>
+        /// Update the pick type, map, spectator type, or allowed summoners for a code.
+        /// </summary>
+        /// <param name="tournamentCode">The tournament code to update</param>
+        /// <param name="allowedSummonerIds">List of summoner id's.</param>
+        /// <param name="spectatorType">The spectator type.</param>
+        /// <param name="pickType">The pick type.</param>
+        /// <param name="mapType">The map type.</param>
         public async void UpdateTournamentCodeAsync(string tournamentCode, List<long> allowedSummonerIds, TournamentSpectatorType? spectatorType, TournamentPickType? pickType, TournamentMapType? mapType)
         {
             var body = BuildTournamentUpdateBody(allowedSummonerIds, spectatorType, pickType, mapType);
