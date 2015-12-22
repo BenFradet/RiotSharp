9f67e7327965a6485bd715adb4f413d3de873714
Joris <jorisvanvugt@live.nl>
2015-12-22 14:57:02 +0100

    Add enum types and their converters for tournament API

---
+++ RiotSharp/TournamentEndpoint/TournamentPickType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotSharp.TournamentEndpoint
+{
+    /// <summary>
+    /// Pick type of the game (Tournament API).
+    /// </summary>
+    public enum TournamentPickType
+    {
+        BlindPick,
+        DraftMode,
+        AllRandom,
+        TournamentDraft
+    }
+}
---
+++ RiotSharp/TournamentEndpoint/TournamentSpectatorType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotSharp.TournamentEndpoint
+{
+    /// <summary>
+    /// Spectator type of the game (Tournament API).
+    /// </summary>
+    public enum TournamentSpectatorType
+    {
+        None,
+        LobbyOnly,
+        All
+    }
+}
