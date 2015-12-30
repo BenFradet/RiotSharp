7c97246ccdedecad9cc30daadb2a7584696858b0
Benjamin Fradet <benjamin.fradet@gmail.com>
2015-12-30 14:49:58 +0100

    turned the project into a PCL

--- RiotSharp/Properties/AssemblyInfo.cs
+++ RiotSharp/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -12,14 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) Benjamin Fradet 2013-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("09051daa-70d5-4a81-8844-1e37e6b1af8a")]
 
 // Version information for an assembly consists of the following four values:
 //
