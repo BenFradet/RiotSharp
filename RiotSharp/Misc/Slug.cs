d0e292f9ead07dcd3c711b4a701e44c5eef8159f
Benjamin Fradet <benjamin.fradet@gmail.com>
2016-02-08 22:59:38 +0100

    slug enum

---
+++ RiotSharp/Misc/Slug.cs
@@ -0,0 +1,59 @@
+﻿
+namespace RiotSharp
+{
+    /// <summary>
+    /// Slug for the Status API.
+    /// </summary>
+    public enum Slug
+    {
+        /// <summary>
+        /// North America.
+        /// </summary>
+        na,
+
+        /// <summary>
+        /// Western Europe.
+        /// </summary>
+        euw,
+
+        /// <summary>
+        /// North-eastern Europe.
+        /// </summary>
+        eune,
+
+        /// <summary>
+        /// Latin America North.
+        /// </summary>
+        lan,
+
+        /// <summary>
+        /// Latin America South.
+        /// </summary>
+        las,
+
+        /// <summary>
+        /// Brasil.
+        /// </summary>
+        br,
+
+        /// <summary>
+        /// Turkey.
+        /// </summary>
+        tr,
+
+        /// <summary>
+        /// Russia.
+        /// </summary>
+        ru,
+
+        /// <summary>
+        /// Oceania.
+        /// </summary>
+        oce,
+
+        /// <summary>
+        /// Japan.
+        /// </summary>
+        jp
+    }
+}
