e58c0b7dedbd18e96b7a13c1093cff5f6988ae37
Ostrea <Ostrea@users.noreply.github.com>
2016-03-23 12:39:44 +0300

    Add documentation
    Add XML documentation for LobbyEvent

--- RiotSharp/TournamentEndpoint/LobbyEvent.cs
+++ RiotSharp/TournamentEndpoint/LobbyEvent.cs
@@ -3,15 +3,45 @@ using Newtonsoft.Json.Converters;
 
 namespace RiotSharp.TournamentEndpoint
 {
+    /// <summary>
+    ///     The type of event that was triggered in the lobby (Tournament API).
+    /// </summary>
     [JsonConverter(typeof(StringEnumConverter))]
     public enum LobbyEvent
     {
+        /// <summary>
+        /// Lobby created.
+        /// </summary>
         PracticeGameCreatedEvent,
+
+        /// <summary>
+        /// Player joins lobby.
+        /// </summary>
         PlayerJoinedGameEvent,
+
+        /// <summary>
+        /// Player switches teams.
+        /// </summary>
         PlayerSwitchedTeamEvent,
+
+        /// <summary>
+        /// Player leaves lobby
+        /// </summary>
         PlayerQuitGameEvent,
+
+        /// <summary>
+        /// Champion selection begins.
+        /// </summary>
         ChampSelectStartedEvent,
+
+        /// <summary>
+        /// Loading screen begins.
+        /// </summary>
         GameAllocationStartedEvent,
+
+        /// <summary>
+        /// Game begins.
+        /// </summary>
         GameAllocatedToLsmEvent
     }
 }
