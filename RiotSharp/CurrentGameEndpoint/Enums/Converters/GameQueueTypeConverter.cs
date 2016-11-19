c2f371e688e11238ea9f7157fc6e3de4f1eb417b
Ben Fradet <benjamin.fradet@gmail.com>
2016-11-19 15:46:34 +0100

    GameQueueType up to date for the current game endpoint, fixes #275 (#276)

--- RiotSharp/CurrentGameEndpoint/Enums/Converters/GameQueueTypeConverter.cs
+++ RiotSharp/CurrentGameEndpoint/Enums/Converters/GameQueueTypeConverter.cs
@@ -23,12 +23,43 @@ namespace RiotSharp.CurrentGameEndpoint.Enums.Converters
                 case "CUSTOM":
                 case "0":
                     return GameQueueType.Custom;
+                case "NORMAL_3x3":
+                case "8":
+                    return GameQueueType.Normal3x3;
                 case "NORMAL_5x5_BLIND":
                 case "2":
                     return GameQueueType.Normal5x5Blind;
+                case "NORMAL_5x5_DRAFT":
+                case "14":
+                    return GameQueueType.Normal5x5Draft;
+                case "RANKED_SOLO_5x5":
+                case "4":
+                    return GameQueueType.RankedSolo5x5;
+                case "RANKED_PREMADE_5x5":
+                case "6":
+                    return GameQueueType.RankedPremade5x5;
+                case "RANKED_PREMADE_3x3":
+                case "RANKED_FLEX_TT":
+                case "9":
+                    return GameQueueType.RankedFlexTT;
+                case "RANKED_TEAM_3x3":
+                case "41":
+                    return GameQueueType.RankedTeam3x3;
+                case "RANKED_TEAM_5x5":
+                case "42":
+                    return GameQueueType.RankedTeam5x5;
+                case "ODIN_5x5_BLIND":
+                case "16":
+                    return GameQueueType.Odin5x5Blind;
+                case "ODIN_5x5_DRAFT":
+                case "17":
+                    return GameQueueType.Odin5x5Draft;
                 case "BOT_5x5":
                 case "7":
                     return GameQueueType.Bot5x5;
+                case "BOT_ODIN_5x5":
+                case "25":
+                    return GameQueueType.BotOdin5x5;
                 case "BOT_5x5_INTRO":
                 case "31":
                     return GameQueueType.Bot5x5Intro;
@@ -38,36 +69,6 @@ namespace RiotSharp.CurrentGameEndpoint.Enums.Converters
                 case "BOT_5x5_INTERMEDIATE":
                 case "33":
                     return GameQueueType.Bot5x5Intermediate;
-                case "NORMAL_3x3":
-                case "8":
-                    return GameQueueType.Normal3x3;
-                case "NORMAL_5x5_DRAFT":
-                case "14":
-                    return GameQueueType.Normal5x5Draft;
-                case "ODIN_5x5_BLIND":
-                case "16":
-                    return GameQueueType.Odin5x5Blind;
-                case "ODIN_5x5_DRAFT":
-                case "17":
-                    return GameQueueType.Odin5x5Draft;
-                case "BOT_ODIN_5x5":
-                case "25":
-                    return GameQueueType.BotOdin5x5;
-                case "RANKED_SOLO_5x5":
-                case "4":
-                    return GameQueueType.RankedSolo5x5;
-                case "RANKED_PREMADE_3x3":
-                case "9":
-                    return GameQueueType.RankedPremade3x3;
-                case "RANKED_PREMADE_5x5":
-                case "6":
-                    return GameQueueType.RankedPremade5x5;
-                case "RANKED_TEAM_3x3":
-                case "41":
-                    return GameQueueType.RankedTeam3x3;
-                case "RANKED_TEAM_5x5":
-                case "42":
-                    return GameQueueType.RankedTeam5x5;
                 case "BOT_TT_3x3":
                 case "52":
                     return GameQueueType.BotTt3x3;
@@ -92,6 +93,9 @@ namespace RiotSharp.CurrentGameEndpoint.Enums.Converters
                 case "URF_5x5":
                 case "76":
                     return GameQueueType.Urf5x5;
+                case "ONEFORALL_MIRRORMODE_5x5":
+                case "78":
+                    return GameQueueType.OneForAllMirrorMode;
                 case "BOT_URF_5x5":
                 case "83":
                     return GameQueueType.BotUrf5x5;
@@ -116,15 +120,33 @@ namespace RiotSharp.CurrentGameEndpoint.Enums.Converters
                 case "KING_PORO_5x5":
                 case "300":
                     return GameQueueType.KingPoro5x5;
+                case "COUNTER_PICK":
+                case "310":
+                    return GameQueueType.CounterPick;
                 case "BILGEWATER_5x5":
                 case "313":
                     return GameQueueType.Bilgewater5x5;
-                case "400":
+                case "SIEGE":
+                case "315":
+                    return GameQueueType.Siege;
+                case "DEFINITELY_NOT_DOMINION_5x5":
+                case "317":
+                    return GameQueueType.DefinitelyNotDominion5x5;
+                case "ARURF_5x5":
+                case "318":
+                    return GameQueueType.ARURF5x5;
                 case "TEAM_BUILDER_DRAFT_UNRANKED_5x5":
-                    return GameQueueType.TeamBuilderDraftUnranked;
-                case "410":
+                case "400":
+                    return GameQueueType.TeamBuilderDraftUnranked5x5;
                 case "TEAM_BUILDER_DRAFT_RANKED_5x5":
-                    return GameQueueType.TeamBuilderDraftRanked;
+                case "410":
+                    return GameQueueType.TeamBuilderDraftRanked5x5;
+                case "TEAM_BUILDER_RANKED_SOLO":
+                case "420":
+                    return GameQueueType.TeamBuilderRankedSolo;
+                case "RANKED_FLEX_SR":
+                case "440":
+                    return GameQueueType.RankedFlexSR;
                 default:
                     return null;
             }
@@ -139,11 +161,41 @@ namespace RiotSharp.CurrentGameEndpoint.Enums.Converters
                 case GameQueueType.Custom:
                     result = "CUSTOM";
                     break;
+                case GameQueueType.Normal3x3:
+                    result = "NORMAL_3x3";
+                    break;
                 case GameQueueType.Normal5x5Blind:
                     result = "NORMAL_5x5_BLIND";
                     break;
+                case GameQueueType.Normal5x5Draft:
+                    result = "NORMAL_5x5_DRAFT";
+                    break;
+                case GameQueueType.RankedSolo5x5:
+                    result = "RANKED_SOLO_5x5";
+                    break;
+                case GameQueueType.RankedPremade5x5:
+                    result = "RANKED_PREMADE_5x5";
+                    break;
+                case GameQueueType.RankedFlexTT:
+                    result = "RANKED_FLEX_TT";
+                    break;
+                case GameQueueType.RankedTeam3x3:
+                    result = "RANKED_TEAM_3x3";
+                    break;
+                case GameQueueType.RankedTeam5x5:
+                    result = "RANKED_TEAM_5x5";
+                    break;
+                case GameQueueType.Odin5x5Blind:
+                    result = "ODIN_5x5_BLIND";
+                    break;
+                case GameQueueType.Odin5x5Draft:
+                    result = "ODIN_5x5_DRAFT";
+                    break;
                 case GameQueueType.Bot5x5:
                     result = "BOT_5x5";
+                    break;
+                case GameQueueType.BotOdin5x5:
+                    result = "BOT_ODIN_5x5";
                     break;
                 case GameQueueType.Bot5x5Intro:
                     result = "BOT_5x5_INTRO";
@@ -153,36 +205,6 @@ namespace RiotSharp.CurrentGameEndpoint.Enums.Converters
                     break;
                 case GameQueueType.Bot5x5Intermediate:
                     result = "BOT_5x5_INTERMEDIATE";
-                    break;
-                case GameQueueType.Normal3x3:
-                    result = "NORMAL_3x3";
-                    break;
-                case GameQueueType.Normal5x5Draft:
-                    result = "NORMAL_5x5_DRAFT";
-                    break;
-                case GameQueueType.Odin5x5Blind:
-                    result = "ODIN_5x5_BLIND";
-                    break;
-                case GameQueueType.Odin5x5Draft:
-                    result = "ODIN_5x5_DRAFT";
-                    break;
-                case GameQueueType.BotOdin5x5:
-                    result = "BOT_ODIN_5x5";
-                    break;
-                case GameQueueType.RankedSolo5x5:
-                    result = "RANKED_SOLO_5x5";
-                    break;
-                case GameQueueType.RankedPremade3x3:
-                    result = "RANKED_PREMADE_3x3";
-                    break;
-                case GameQueueType.RankedPremade5x5:
-                    result = "RANKED_PREMADE_5x5";
-                    break;
-                case GameQueueType.RankedTeam3x3:
-                    result = "RANKED_TEAM_3x3";
-                    break;
-                case GameQueueType.RankedTeam5x5:
-                    result = "RANKED_TEAM_5x5";
                     break;
                 case GameQueueType.BotTt3x3:
                     result = "BOT_TT_3x3";
@@ -208,6 +230,9 @@ namespace RiotSharp.CurrentGameEndpoint.Enums.Converters
                 case GameQueueType.Urf5x5:
                     result = "URF_5x5";
                     break;
+                case GameQueueType.OneForAllMirrorMode:
+                    result = "ONEFORALL_MIRRORMODE_5x5";
+                    break;
                 case GameQueueType.BotUrf5x5:
                     result = "BOT_URF_5x5";
                     break;
@@ -232,14 +257,32 @@ namespace RiotSharp.CurrentGameEndpoint.Enums.Converters
                 case GameQueueType.KingPoro5x5:
                     result = "KING_PORO_5x5";
                     break;
+                case GameQueueType.CounterPick:
+                    result = "COUNTER_PICK";
+                    break;
                 case GameQueueType.Bilgewater5x5:
                     result = "BILGEWATER_5x5";
                     break;
-                case GameQueueType.TeamBuilderDraftUnranked:
+                case GameQueueType.Siege:
+                    result = "SIEGE";
+                    break;
+                case GameQueueType.DefinitelyNotDominion5x5:
+                    result = "DEFINITELY_NOT_DOMINION_5x5";
+                    break;
+                case GameQueueType.ARURF5x5:
+                    result = "ARURF_5x5";
+                    break;
+                case GameQueueType.TeamBuilderDraftUnranked5x5:
                     result = "TEAM_BUILDER_DRAFT_UNRANKED_5x5";
                     break;
-                case GameQueueType.TeamBuilderDraftRanked:
+                case GameQueueType.TeamBuilderDraftRanked5x5:
                     result = "TEAM_BUILDER_DRAFT_RANKED_5x5";
+                    break;
+                case GameQueueType.TeamBuilderRankedSolo:
+                    result = "TEAM_BUILDER_RANKED_SOLO";
+                    break;
+                case GameQueueType.RankedFlexSR:
+                    result = "RANKED_FLEX_SR";
                     break;
                 default:
                     result = "";
