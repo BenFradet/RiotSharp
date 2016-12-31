acd7376259efe3ea061628f7cd317e17ed533334
Mingwei Samuel <mingwei.samuel@gmail.com>
2016-12-31 08:54:26 -0800

    adding MonsterSubType enum & field (#310)
    fixes #309

---
+++ RiotSharp/MatchEndpoint/Enums/MonsterSubType.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using RiotSharp.MatchEndpoint.Enums.Converters;
+
+namespace RiotSharp.MatchEndpoint.Enums
+{
+    /// <summary>
+    /// Type of monster (Match API).
+    /// </summary>
+    [JsonConverter(typeof(MonsterSubTypeConverter))]
+    public enum MonsterSubType
+    {
+        /// <summary>
+        /// Corresponds to the cloud drake.
+        /// </summary>
+        AirDragon,
+
+        /// <summary>
+        /// Corresponds to the ocean drake.
+        /// </summary>
+        WaterDragon,
+
+        /// <summary>
+        /// Corresponds to the mountain drake.
+        /// </summary>
+        EarthDragon,
+
+        /// <summary>
+        /// Corresponds to the infernal drake.
+        /// </summary>
+        FireDragon,
+
+        /// <summary>
+        /// Corresponds to the elder dragon.
+        /// </summary>
+        ElderDragon
+    }
+
+    static class MonsterSubTypeExtension
+    {
+        public static string ToCustomString(this MonsterSubType MonsterSubType)
+        {
+            switch (MonsterSubType)
+            {
+                case MonsterSubType.AirDragon:
+                    return "AIR_DRAGON";
+                case MonsterSubType.WaterDragon:
+                    return "WATER_DRAGON";
+                case MonsterSubType.EarthDragon:
+                    return "EARTH_DRAGON";
+                case MonsterSubType.FireDragon:
+                    return "FIRE_DRAGON";
+                case MonsterSubType.ElderDragon:
+                    return "ELDER_DRAGON";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
