99a73b65274c542d6dc0fa80e2eee4970849802d
Ben Fradet <benjamin.fradet@gmail.com>
2017-02-26 21:56:54 +0100

    2.5.3

--- RiotSharp/Properties/AssemblyInfo.cs
+++ RiotSharp/Properties/AssemblyInfo.cs
@@ -24,4 +24,4 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.0.*")]
+[assembly: AssemblyVersion("2.5.3.*")]
