012b80f536d7b2ef30e965b51e452f762a5387b7
Jan Ouborny <jan.ouborny@Obungi.com>
2017-04-14 22:32:23 +0200

    Changed type of Queue from enum to string (#307)

---
+++ RiotSharp/MatchListEndPoint/Queue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotSharp
+{
+    public static class Queue
+    {
+
+        /// <summary>
+        /// Solo queue 5 vs 5.
+        /// </summary>
+        public const string RankedSolo5x5 = "RANKED_SOLO_5x5";
+
+        /// <summary>
+        /// Team 3 vs 3.
+        /// </summary>
+        public const string RankedTeam3x3 = "RANKED_TEAM_3x3";
+
+        /// <summary>
+        /// Team 5 vs 5.
+        /// </summary>
+        public const string RankedTeam5x5 = "RANKED_TEAM_5x5";
+
+        /// <summary>
+        /// Team 5 v 5 - Dynamic Queue - Ranked
+        /// </summary>
+        public const string TeamBuilderDraftRanked5x5 = "TEAM_BUIDER_DRAFT_RANKED_5x5";
+
+        /// <summary>
+        /// Team 5 v 5 - Dynamic Queue - Unranked
+        /// </summary>
+        public const string TeamBuilderDraftUnranked5x5 = "TEAM_BUILDER_DRAFT_UNRANKED_5x5";
+
+        /// <summary>
+        /// New Summoner's Rift ranked games.
+        /// </summary>
+        public const string RankedFlexSR = "RANKED_FLEX_SR";
+
+        /// <summary>
+        /// New Twisted Treeline ranked games.
+        /// </summary>
+        public const string RankedFlexTT = "RANKED_FLEX_TT";
+
+        /// <summary>
+        /// Ranked Solo games from current season that use Team Builder matchmaking
+        /// </summary>
+        public const string TeamBuilderRankedSolo = "TEAM_BUILDER_RANKED_SOLO";
+
+        /// <summary>
+        /// Used for both historical Ranked Premade 3v3 games
+        /// </summary>
+        public const string RankedPremade3x3 = "RANKED_PREMADE_3x3";
+
+        /// <summary>
+        /// Ranked Premade 5v5 games
+        /// </summary>
+        public const string RankedPremade5x5 = "RANKED_PREMADE_5x5";
+    }
+}
