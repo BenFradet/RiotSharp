7a6fdc2cbf155e081ad960faf3044f9715e77ac5
Jan Ouborny <j.ouborny@web.de>
2017-04-16 19:09:13 +0200

    Added CultureInfo extension method ToLanguage (#357)

---
+++ RiotSharp/Misc/CultureInfoExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotSharp
+{
+    public static class CultureInfoExtensions
+    {
+        public static Language ToLanguage(this CultureInfo cultureInfo)
+        {
+            if (cultureInfo.IsNeutralCulture) // Neutral cultures don't have an equivalent in Riot's API, therefore they need to be mapped to a default
+            {
+                return ParseNeutralCulture(cultureInfo);
+            }
+            else
+            {
+                var riotLanguageName = cultureInfo.Name.Replace('-', '_');
+                Language language = Language.en_US;
+                if (Enum.TryParse(riotLanguageName, out language))
+                {
+                    return language;
+                }
+                else
+                {
+                    return ParseNeutralCulture(cultureInfo.Parent); // Parent culture is always neutral (as far as the supported languages go)
+                }
+            }
+        }
+
+        private static Language ParseNeutralCulture(CultureInfo cultureInfo)
+        {
+            switch (cultureInfo.TwoLetterISOLanguageName)
+            {
+                case "cs":
+                    return Language.cs_CZ;
+                case "de":
+                    return Language.de_DE;
+                case "el":
+                    return Language.el_GR;
+                case "en":
+                    return Language.en_US;                
+                case "es":
+                    return Language.es_ES;
+                case "fr":
+                    return Language.fr_FR;
+                case "hu":
+                    return Language.hu_HU;
+                case "id":
+                    return Language.id_ID;
+                case "it":
+                    return Language.it_IT;
+                case "ja":
+                    return Language.ja_JP;
+                case "ko":
+                    return Language.ko_KR;
+                case "ms":
+                    return Language.ms_MY;
+                case "pl":
+                    return Language.pl_PL;
+                case "pt":
+                    return Language.pt_BR;
+                case "ro":
+                    return Language.ro_RO;
+                case "ru":
+                    return Language.ru_RU;
+                case "th":
+                    return Language.th_TH;
+                case "tr":
+                    return Language.tr_TR;
+                case "vn":
+                    return Language.vn_VN;
+                case "zh":
+                    return Language.zh_CN;
+                default:
+                    throw new ArgumentException("Unsupported language:" + cultureInfo.Name);
+            }
+        }
+    }
+}
---
+++ RiotSharpTest/Misc/CultureInfoExtensionsTest.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RiotSharp;
+
+namespace RiotSharpTest
+{
+    [TestClass]
+    public class CultureInfoExtensionsTest
+    {
+        private static List<CultureInfo> supportedNeutralCultureInfos = new List<CultureInfo>
+        {
+            new CultureInfo("cs"), new CultureInfo("de"), new CultureInfo("el"), new CultureInfo("en"),
+            new CultureInfo("es"), new CultureInfo("fr"), new CultureInfo("hu"), new CultureInfo("id"),
+            new CultureInfo("it"), new CultureInfo("ja"), new CultureInfo("ko"), new CultureInfo("ms"), 
+            new CultureInfo("pl"), new CultureInfo("pt"), new CultureInfo("ro"), new CultureInfo("ru"), 
+            new CultureInfo("th"), new CultureInfo("tr"), new CultureInfo("vn"), new CultureInfo("zh") 
+        };
+        private static List<CultureInfo> directlySupportedCultureInfos = new List<CultureInfo>
+        {
+            new CultureInfo("cs-CZ"), new CultureInfo("de-DE"), new CultureInfo("el-GR"), new CultureInfo("en-AU"),
+            new CultureInfo("en-GB"), new CultureInfo("en-PH"), new CultureInfo("en-PL"), new CultureInfo("en-SG"),
+            new CultureInfo("en-US"), new CultureInfo("es-AR"), new CultureInfo("es-ES"), new CultureInfo("es-MX"),
+            new CultureInfo("fr-FR"), new CultureInfo("hu-HU"), new CultureInfo("id-ID"), new CultureInfo("it-IT"),
+            new CultureInfo("ja-JP"), new CultureInfo("ko-KR"), new CultureInfo("ms-MY"), new CultureInfo("pl-PL"),
+            new CultureInfo("pt-BR"), new CultureInfo("ro-RO"), new CultureInfo("ru-RU"), new CultureInfo("th-TH"),
+            new CultureInfo("tr-TR"), new CultureInfo("vn-VN"), new CultureInfo("zh-CN"), new CultureInfo("zh-MY"),
+            new CultureInfo("zh-TW")
+        };
+        private static CultureInfo unsupportedNeutralCultureInfo = new CultureInfo("et");
+        private static CultureInfo unsupportedCultureInfo = new CultureInfo("et-ET");
+
+        [TestMethod]
+        public void ToLanguage_SupportedNeutralCultureInfo_ReturnsLanguage()
+        {
+            foreach(var neutralCulture in supportedNeutralCultureInfos)
+            {               
+                try
+                {
+                    // Act
+                    var language = neutralCulture.ToLanguage();
+                }
+                catch
+                {
+                    // Assert
+                    Assert.Fail("Conversion of " + neutralCulture.Name + " failed.");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ToLanguage_DirectlySupportedCultureInfo_ReturnsLanguage()
+        {
+            foreach (var directlySupportedCulture in directlySupportedCultureInfos)
+            {
+                try
+                {
+                    // Act
+                    var language = directlySupportedCulture.ToLanguage();
+                }
+                catch
+                {
+                    // Assert
+                    Assert.Fail("Conversion of " + directlySupportedCulture.Name + " failed.");
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ToLanguage_UnsupportedNeutralCultureInfo_ThrowsArgumentException()
+        {
+            // Act and Assert - Expects exception
+            var language = unsupportedNeutralCultureInfo.ToLanguage();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ToLanguage_UnsupportedCultureInfo_ThrowsArgumentExcepion()
+        {
+            // Act and Assert - Expects exception
+            var language = unsupportedNeutralCultureInfo.ToLanguage();
+        }
+    }
+}
