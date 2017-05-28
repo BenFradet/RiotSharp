7621deb5827dc752964a2b8f61735906d00dcac7
Jan Ouborny <j.ouborny@web.de>
2017-05-28 19:08:15 +0200

    Added AspNetCore DI setup method, fixes #381 (#410)

---
+++ RiotSharp.AspNetCore/ApiKey.cs
@@ -0,0 +1,9 @@
+﻿namespace RiotSharp.AspNetCore
+{
+    public class ApiKey
+    {
+        public string Key { get; set; }
+        public int RateLimitPer10S { get; set; }
+        public int RateLimitPer10M { get; set; }
+    }
+}
