e08141ecfada5b45dedafdc3b83b07721825869b
Jan Ouborny <j.ouborny@web.de>
2017-06-07 00:59:50 +0200

    Added xunit test project

---
+++ RiotSharp.Test/CommonTestBase.cs
@@ -0,0 +1,81 @@
+﻿//using System;
+//using System.Configuration;
+//using RiotSharp.Misc;
+//using RiotSharp;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System.Net;
+
+//namespace RiotSharpTest
+//{
+//    public class CommonTestBase
+//    {
+//        public static string apiKey = ConfigurationManager.AppSettings["ApiKey"];
+//        public static string faultyApiKey = ConfigurationManager.AppSettings["FaultyApiKey"];
+
+//        public static Region summoner1and2Region = (Region)Enum.Parse(typeof(Region),
+//            ConfigurationManager.AppSettings["Summoner1and2Region"]);
+
+//        public static long invalidSummonerId = -1;
+//        public static long summoner1Id = long.Parse(ConfigurationManager.AppSettings["Summoner1Id"]);
+//        public static long summoner1AccountId = long.Parse(ConfigurationManager.AppSettings["Summoner1AccountId"]);
+//        public static string summoner1Name = ConfigurationManager.AppSettings["Summoner1Name"];
+
+//        public static long summoner2Id = long.Parse(ConfigurationManager.AppSettings["Summoner2Id"]);
+//        public static long summoner2AccountId = long.Parse(ConfigurationManager.AppSettings["Summoner2AccountId"]);
+//        public static string summoner2Name = ConfigurationManager.AppSettings["Summoner2Name"];
+
+//        /// <summary>
+//        /// Ignores the test if the server responds with 429 or 500
+//        /// </summary>
+//        /// <param name="action"></param>
+//        protected void EnsureCredibility(Action action)
+//        {
+//            try
+//            {
+//                action();
+//            }
+//            catch (RiotSharpException exception)
+//            {
+//                HandleRiotSharpException(exception);
+//            }
+//            // Catches exception thrown by async methods
+//            catch (AggregateException exception)
+//            {
+//                HandleAggregateException(exception);
+//                throw exception;
+//            }
+//        }
+
+//        protected void HandleAggregateException(AggregateException exception, Action<RiotSharpException> riotSharpExceptionHandler = null)
+//        {
+//            if (exception.InnerException != null)
+//            {
+//                if (exception.InnerException.GetType() == typeof(RiotSharpException))
+//                {
+//                    if (riotSharpExceptionHandler == null)
+//                        HandleRiotSharpException((RiotSharpException)exception.InnerException);
+//                    else
+//                        riotSharpExceptionHandler((RiotSharpException)exception.InnerException);
+//                }
+//                else if (exception.InnerException.GetType() == typeof(AggregateException))
+//                    HandleAggregateException((AggregateException)exception.InnerException, riotSharpExceptionHandler);
+//                else
+//                    return; // Go back to root to throw root exception
+//            }
+//            else
+//                return;
+//        }
+
+//        private void HandleRiotSharpException(RiotSharpException exception)
+//        {
+//            if (exception.HttpStatusCode == HttpStatusCode.InternalServerError)
+//                Assert.Inconclusive("Internal server error (500).");
+//            else if (exception.HttpStatusCode == (HttpStatusCode)429)
+//                Assert.Inconclusive("Rate limit exceeded (429).");
+//            else if (exception.HttpStatusCode == HttpStatusCode.ServiceUnavailable)
+//                Assert.Inconclusive("Service is unavailable (503).");
+//            else
+//                throw exception;
+//        }
+//    }
+//}
---
+++ RiotSharp.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using RiotSharp;
+
+
+namespace RiotSharp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
