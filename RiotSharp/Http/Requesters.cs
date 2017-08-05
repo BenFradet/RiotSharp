33a72dbda9321ad157e10a44d2c9c679944333ea
Jan Ouborny <j.ouborny@web.de>
2017-08-05 14:20:56 +0200

    Add rate limiting for the static api

--- RiotSharp/Http/Requesters.cs
+++ RiotSharp/Http/Requesters.cs
@@ -2,7 +2,7 @@
 {
     internal static class Requesters
     {
-        public static Requester StaticApiRequester;
+        public static RateLimitedRequester StaticApiRequester;
         public static Requester StatusApiRequester;
         public static RateLimitedRequester RiotApiRequester;
         public static RateLimitedRequester TournamentApiRequester;
