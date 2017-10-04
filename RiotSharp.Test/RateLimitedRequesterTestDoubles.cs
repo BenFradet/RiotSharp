099e3af262024c3259a3da6440fba70aeecf76c0
Conall <newmanconall@gmail.com>
2017-10-04 02:05:29 +0100

    Fix status api test dependency on requester

---
+++ RiotSharp.Test/RateLimitedRequesterTestDoubles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using RiotSharp.Http;
+using RiotSharp.Http.Interfaces;
+
+namespace RiotSharp.Test
+{
+    class RateLimitedRequesterTestDoubles
+    {
+        public static IRateLimitedRequester Real(string apiKey, IDictionary<TimeSpan, int> rateLimits)
+        {
+            var serializer = new RequestContentSerializer();
+            var deserializer = new ResponseDeserializer();
+            var requestCreator = new RequestCreator(apiKey, serializer);
+            var httpClient = new HttpClient();
+            var failedRequestHandler = new FailedRequestHandler();
+            var client = new RequestClient(httpClient, failedRequestHandler);
+            var basicRequester = new Requester(client, requestCreator, deserializer);
+            var rateLimitProvider = new RateLimitProvider(rateLimits);
+
+            return new RateLimitedRequester(basicRequester, rateLimitProvider);
+        }
+
+        public static IRateLimitedRequester Real(string apiKey)
+        {
+            var rateLimits = new Dictionary<TimeSpan, int> { { new TimeSpan(1, 0, 0), 10 } };
+
+            return Real(apiKey, rateLimits);
+        }
+    }
+}
---
+++ RiotSharp.Test/RequesterTestDoubles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using RiotSharp.Http;
+using RiotSharp.Http.Interfaces;
+
+namespace RiotSharp.Test
+{
+    public class RequesterTestDoubles
+    {
+        public static IRequester Real(string apiKey)
+        {
+            var serializer = new RequestContentSerializer();
+            var deserializer = new ResponseDeserializer();
+            var requestCreator = new RequestCreator(apiKey, serializer);
+
+            var httpClient = new HttpClient();
+            var failedRequestHandler = new FailedRequestHandler();
+
+            var client = new RequestClient(httpClient, failedRequestHandler);
+
+            return new Requester(client, requestCreator, deserializer);
+        }
+    }
+}
