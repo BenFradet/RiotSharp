83d9a2a86b25ab002b820af9560e3c1a10291d8d
Jan Ouborny <j.ouborny@web.de>
2017-11-22 23:17:59 +0100

    Add api key options

--- RiotSharp.AspNetCore/ApiKeyOptions.cs
+++ RiotSharp.AspNetCore/ApiKeyOptions.cs
@@ -14,10 +14,20 @@ namespace RiotSharp.AspNetCore
         /// Enable or disable default caching for the static data endpint
         /// </summary>
         public bool UseCache { get; set; }
+
         /// <summary>
-        /// Enable or disable ASP.NET Core memory cache implementation for caching
+        /// Enable or disable ASP.NET Core in-memory cache implementation for caching
         /// </summary>
         public bool UseMemoryCache { get; set; }
+        /// <summary>
+        /// Enable or disable ASP.NET Core distributed cache implementation for caching
+        /// </summary>
+        public bool UseDistributedCache { get; set; }
+        /// <summary>
+        /// Enable or disable memory cache and distributed cache. If enabled, in-memory cache will be at first and distributed as a fallback.
+        /// </summary>
+        public bool UseHybridCache { get; set; }
+
         /// <summary>
         /// Sliding expiration time for caching of the static data endpoint
         /// </summary>
