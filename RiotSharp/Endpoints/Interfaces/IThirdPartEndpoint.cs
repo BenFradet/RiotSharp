7d4b994c187770c9ac7ff9a2bba6ee016d69f8f3
SMARTSOFTDCS\oucema.zitoun <oucema.zitoun@esprit.tn>
2018-01-19 12:19:39 +0300

    Implemented the third party endpoint.
    Add third-party-code-v3 Endpoint #500

---
+++ RiotSharp/Endpoints/Interfaces/IThirdPartEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using RiotSharp.Misc;
+
+namespace RiotSharp.Endpoints.Interfaces
+{
+    /// <summary>
+    /// The Third Party Endpoint.
+    /// </summary>
+    public interface IThirdPartEndpoint
+    {
+        /// <summary>
+        /// Get a thrid party code by summoner id synchronously.
+        /// </summary>
+        /// <param name="region">Region in which you wish to look for a summoner.</param>
+        /// <param name="summonerId">Id of the summoner you're looking for.</param>
+        /// <returns>A string.</returns>
+        string GetThirdPartyCodeBySummonerId(Region region, long summonerId);
+
+        /// <summary>
+        /// Get a thrid party code by summoner id asynchronously.
+        /// </summary>
+        /// <param name="region">Region in which you wish to look for a summoner.</param>
+        /// <param name="summonerId">Id of the summoner you're looking for.</param>
+        /// <returns>A string.</returns>
+        Task<string> GetThirdPartyCodeBySummonerIdAsync(Region region, long summonerId);
+    }
+}
---
+++ RiotSharp/Endpoints/ThirdPartEndpoint/ThirdPartyEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using RiotSharp.Endpoints.Interfaces;
+using RiotSharp.Http.Interfaces;
+using RiotSharp.Misc;
+
+namespace RiotSharp.Endpoints.ThirdPartEndpoint
+{
+    public class ThirdPartyEndpoint : IThirdPartEndpoint
+    {
+        private const string ThirdPartyRootUrl = "/lol/platform/v3/third-party-code";
+        private const string ThirdPartyBySummonerUrl = "/by-summoner/{0}";
+
+        private readonly IRateLimitedRequester _requester;
+
+        public ThirdPartyEndpoint(IRateLimitedRequester requester)
+        {
+            _requester = requester;
+        }
+
+
+        public string GetThirdPartyCodeBySummonerId(Region region, long summonerId)
+        {
+            return _requester.CreateGetRequest(ThirdPartyRootUrl + 
+                string.Format(ThirdPartyBySummonerUrl, summonerId),
+                region);
+        }
+
+        public async Task<string> GetThirdPartyCodeBySummonerIdAsync(Region region, long summonerId)
+        {
+            return await _requester.CreateGetRequestAsync(
+                string.Format(ThirdPartyRootUrl + ThirdPartyBySummonerUrl, summonerId),region);
+        }
+    }
+}
