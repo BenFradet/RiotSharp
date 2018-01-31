79bd15697c04cd30d8c6294b0b56346e68a678da
Jan Ouborny <j.ouborny@web.de>
2018-01-31 19:38:12 +0100

    Remove synchronous GetThirdPartyCodeBySummonerId method

--- RiotSharp/Endpoints/Interfaces/IThirdPartEndpoint.cs
+++ RiotSharp/Endpoints/Interfaces/IThirdPartEndpoint.cs
@@ -9,14 +9,6 @@ namespace RiotSharp.Endpoints.Interfaces
     public interface IThirdPartEndpoint
     {
         /// <summary>
-        /// Get a thrid party code by summoner id synchronously.
-        /// </summary>
-        /// <param name="region">Region in which you wish to look for a summoner.</param>
-        /// <param name="summonerId">Id of the summoner you're looking for.</param>
-        /// <returns>A string.</returns>
-        string GetThirdPartyCodeBySummonerId(Region region, long summonerId);
-
-        /// <summary>
         /// Get a thrid party code by summoner id asynchronously.
         /// </summary>
         /// <param name="region">Region in which you wish to look for a summoner.</param>
