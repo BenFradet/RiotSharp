054ee2977e63ae758585ba946b6893d58ff1dc3e
Jan Ouborny <j.ouborny@web.de>
2018-02-11 23:23:10 +0100

    Fix Json deserialization bug of static wrappers

--- RiotSharp/Endpoints/StaticDataEndpoint/Map/Cache/MapsStaticWrapper.cs
+++ RiotSharp/Endpoints/StaticDataEndpoint/Map/Cache/MapsStaticWrapper.cs
@@ -1,9 +1,11 @@
-﻿using RiotSharp.Misc;
+﻿using Newtonsoft.Json;
+using RiotSharp.Misc;
 
 namespace RiotSharp.Endpoints.StaticDataEndpoint.Map.Cache
 {
     class MapsStaticWrapper
     {
+        [JsonProperty]
         public MapsStatic MapsStatic { get; private set; }
         public Language Language { get; private set; }
         public string Version { get; private set; }
--- RiotSharp/Endpoints/StaticDataEndpoint/Mastery/Cache/MasteryStaticWrapper.cs
+++ RiotSharp/Endpoints/StaticDataEndpoint/Mastery/Cache/MasteryStaticWrapper.cs
@@ -1,9 +1,11 @@
-﻿using RiotSharp.Misc;
+﻿using Newtonsoft.Json;
+using RiotSharp.Misc;
 
 namespace RiotSharp.Endpoints.StaticDataEndpoint.Mastery.Cache
 {
     class MasteryStaticWrapper
     {
+        [JsonProperty]
         public MasteryStatic MasteryStatic { get; private set; }
         public Language Language { get; private set; }
         public MasteryData MasteryData { get; private set; }
