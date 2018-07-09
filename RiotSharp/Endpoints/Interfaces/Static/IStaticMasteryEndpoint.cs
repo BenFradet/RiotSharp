94135707f148f96b02ae4a7583b1405f18d78fd2
Jan Ouborny <j.ouborny@web.de>
2018-07-09 19:39:01 +0200

    Migrate mastery endpoint

--- RiotSharp/Endpoints/Interfaces/Static/IStaticMasteryEndpoint.cs
+++ RiotSharp/Endpoints/Interfaces/Static/IStaticMasteryEndpoint.cs
@@ -10,26 +10,9 @@ namespace RiotSharp.Endpoints.Interfaces.Static
         /// <summary>
         /// Get a list of all masteries asynchronously.
         /// </summary>
-        /// <param name="region">Region from which to retrieve the data.</param>
-        /// <param name="masteryData">Data to retrieve.</param>
+        /// <param name="version">Patch version for returned data.</param>
         /// <param name="language">Language of the data to be retrieved.</param>
-        /// <param name="version">Patch version for returned data. If not specified, the latest version for the region is used. 
-        /// List of valid versions can be obtained from the /versions endpoint.</param>
         /// <returns>An MasteryListStatic object containing all masteries.</returns>
-        Task<MasteryListStatic> GetMasteriesAsync(Region region,
-            MasteryData masteryData = MasteryData.All, Language language = Language.en_US, string version = null);
-
-        /// <summary>
-        /// Get a mastery asynchronously.
-        /// </summary>
-        /// <param name="region">Region from which to retrieve the data.</param>
-        /// <param name="masteryId">Id of the mastery to retrieve.</param>
-        /// <param name="masteryData">Data to retrieve.</param>
-        /// <param name="language">Language of th data to be retrieved.</param>
-        /// <param name="version">Patch version for returned data. If not specified, the latest version for the region is used. 
-        /// List of valid versions can be obtained from the /versions endpoint.</param>
-        /// <returns>A mastery.</returns>
-        Task<MasteryStatic> GetMasteryAsync(Region region, int masteryId,
-            MasteryData masteryData = MasteryData.All, Language language = Language.en_US, string version = null);
+        Task<MasteryListStatic> GetMasteriesAsync(string version, Language language = Language.en_US);
     }
 }
--- RiotSharp/Endpoints/StaticDataEndpoint/Mastery/Cache/MasteryListStaticWrapper.cs
+++ RiotSharp/Endpoints/StaticDataEndpoint/Mastery/Cache/MasteryListStaticWrapper.cs
@@ -8,13 +8,13 @@ namespace RiotSharp.Endpoints.StaticDataEndpoint.Mastery.Cache
         [JsonProperty]
         public MasteryListStatic MasteryListStatic { get; private set; }
         public Language Language { get; private set; }
-        public MasteryData MasteryData { get; private set; }
+        public string Version { get; private set; }
 
-        public MasteryListStaticWrapper(MasteryListStatic masteries, Language language, MasteryData masteryData)
+        public MasteryListStaticWrapper(MasteryListStatic masteries, Language language, string version)
         {
             MasteryListStatic = masteries;
             Language = language;
-            MasteryData = masteryData;
+            Version = version;
         }
     }
 }
