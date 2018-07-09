09b0ffe8871c58d56547d364bcf76a4414d6edad
Jan Ouborny <j.ouborny@web.de>
2018-07-09 19:38:38 +0200

    Migrate reforged runes endpoint

--- RiotSharp/Endpoints/Interfaces/Static/IStaticReforgedRuneEndpoint.cs
+++ RiotSharp/Endpoints/Interfaces/Static/IStaticReforgedRuneEndpoint.cs
@@ -10,46 +10,11 @@ namespace RiotSharp.Endpoints.Interfaces.Static
     public interface IStaticReforgedRuneEndpoint : IStaticEndpoint
     {
         /// <summary>
-        /// Get a reforged rune asynchronously.
-        /// </summary>
-        /// <param name="region">Region from which to retrieve the data.</param>
-        /// <param name="reforgedRuneId">Id of the reforged rune to retrieve.</param>
-        /// <param name="language">Language of the data to be retrieved.</param>
-        /// <param name="version">Patch version for returned data. If not specified, the latest version for the region is used. 
-        /// List of valid versions can be obtained from the /versions endpoint.</param>
-        /// <returns>A summoner spell.</returns>
-        Task<ReforgedRuneStatic> GetReforgedRuneAsync(Region region, int reforgedRuneId, Language language = Language.en_US, string version = null);
-
-        /// <summary>
         /// Get a list of all reforged runes asynchronously.
         /// </summary>
-        /// <param name="region">Region from which to retrieve the data.</param>
+        /// <param name="version">Patch version for returned data.</param>
         /// <param name="language">Language of the data to be retrieved.</param>
-        /// <param name="version">Patch version for returned data. If not specified, the latest version for the region is used. 
-        /// List of valid versions can be obtained from the /versions endpoint.</param>
         /// <returns>A summoner spell.</returns>
-        Task<List<ReforgedRuneStatic>> GetReforgedRunesAsync(Region region, Language language = Language.en_US, string version = null);
-
-        /// <summary>
-        /// Get a reforged rune path asynchronously.
-        /// </summary>
-        /// <param name="region">Region from which to retrieve the data.</param>
-        /// <param name="reforgedRunePathId">Id of the reforged rune path to retrieve.</param>
-        /// <param name="language">Language of the data to be retrieved.</param>
-        /// <param name="version">Patch version for returned data. If not specified, the latest version for the region is used. 
-        /// List of valid versions can be obtained from the /versions endpoint.</param>
-        /// <returns>A summoner spell.</returns>
-        Task<ReforgedRunePathStatic> GetReforgedRunePathAsync(Region region, int reforgedRunePathId,
-            Language language = Language.en_US, string version = null);
-
-        /// <summary>
-        /// Get a list of all reforged rune paths asynchronously.
-        /// </summary>
-        /// <param name="region">Region from which to retrieve the data.</param>
-        /// <param name="language">Language of the data to be retrieved.</param>
-        /// <param name="version">Patch version for returned data. If not specified, the latest version for the region is used. 
-        /// List of valid versions can be obtained from the /versions endpoint.</param>
-        /// <returns>A summoner spell.</returns>
-        Task<List<ReforgedRunePathStatic>> GetReforgedRunePathsAsync(Region region, Language language = Language.en_US, string version = null);
+        Task<List<ReforgedRuneStatic>> GetReforgedRunesAsync(string version, Language language = Language.en_US);
     }
 }
