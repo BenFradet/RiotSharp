58fd4c436e0750ec4bcf95a79b2859416d614a7a
dsychin <desmond@shiunyean.tk>
2018-11-16 22:18:02 +0800

    removed url and related functions for deprecated champion endpoint

--- RiotSharp/Endpoints/ChampionEndpoint/ChampionEndpoint.cs
+++ RiotSharp/Endpoints/ChampionEndpoint/ChampionEndpoint.cs
@@ -14,7 +14,6 @@ namespace RiotSharp.Endpoints.ChampionEndpoint
     public class ChampionEndpoint : IChampionEndpoint
     {
         private const string PlatformRootUrl = "/lol/platform/v3";
-        private const string ChampionsUrl = "/champions";
         private const string ChampionRotationUrl = "/champion-rotations";
         private const string IdUrl = "/{0}";
 
@@ -27,22 +26,6 @@ namespace RiotSharp.Endpoints.ChampionEndpoint
         public ChampionEndpoint(IRateLimitedRequester requester)
         {
             _requester = requester;
-        }
-
-        /// <inheritdoc />
-        public async Task<List<Champion>> GetChampionsAsync(Region region, bool freeToPlay = false)
-        {
-            var json = await _requester.CreateGetRequestAsync(PlatformRootUrl + ChampionsUrl, region,
-                new List<string> { $"freeToPlay={freeToPlay.ToString().ToLower()}" }).ConfigureAwait(false);
-            return JsonConvert.DeserializeObject<ChampionList>(json).Champions;
-        }
-
-        /// <inheritdoc />
-        public async Task<Champion> GetChampionAsync(Region region, int championId)
-        {
-            var json = await _requester.CreateGetRequestAsync(
-                PlatformRootUrl + ChampionsUrl + string.Format(IdUrl, championId), region).ConfigureAwait(false);
-            return JsonConvert.DeserializeObject<Champion>(json);
         }
 
         /// <inheritdoc />
