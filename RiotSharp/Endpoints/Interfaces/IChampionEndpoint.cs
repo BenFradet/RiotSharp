77685912ddbb01e335050f1ae2fb8831aa94c532
Desmond Chin <chin.sy.des@gmail.com>
2018-11-19 16:17:07 +0800

    Remove the deprecated champion endpoint, fixes #581 (#590)

--- RiotSharp/Endpoints/Interfaces/IChampionEndpoint.cs
+++ RiotSharp/Endpoints/Interfaces/IChampionEndpoint.cs
@@ -11,22 +11,6 @@ namespace RiotSharp.Endpoints.Interfaces
     public interface IChampionEndpoint
     {
         /// <summary>
-        /// Get the list of champions by region asynchronously.
-        /// </summary>
-        /// <param name="region">Region in which you wish to look for champions.</param>
-        /// <param name="freeToPlay">If set to true will return only free to play champions.</param>
-        /// <returns>A list of champions.</returns>
-        Task<List<Champion>> GetChampionsAsync(Region region, bool freeToPlay = false);
-
-        /// <summary>
-        /// Get a champion from its id asynchronously.
-        /// </summary>
-        /// <param name="region">Region in which you wish to look for a champion.</param>
-        /// <param name="championId">Id of the champion you're looking for.</param>
-        /// <returns>A champion.</returns>
-        Task<Champion> GetChampionAsync(Region region, int championId);
-
-        /// <summary>
         /// Get the list of free champions by region asynchronously.
         /// </summary>
         /// <param name="region">Region in which you wish to look for champion rotation.</param>
