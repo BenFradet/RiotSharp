18e5c3e8315a14f094d5e9fd4d61a7acdbef7c25
Jan Ouborny <j.ouborny@web.de>
2019-04-12 20:19:55 +0200

    Add additional unit tests for match endpoint

---
+++ RiotSharp.Test/EndpointTests/MatchEndpointTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RiotSharp.Caching;
+using RiotSharp.Endpoints.Interfaces;
+using RiotSharp.Endpoints.MatchEndpoint;
+using RiotSharp.Http.Interfaces;
+using RiotSharp.Misc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RiotSharp.Test.EndpointTests
+{
+    [TestClass]
+    public class MatchEndpointTests
+    {
+        private Mock<IRateLimitedRequester> _rateLimitedRequester;
+        private IMatchEndpoint _matchEndpoint;
+
+        private const string ResponsePath = "./Resources/MatchEndpoint/MatchList_EUW_Response.txt";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _rateLimitedRequester = new Mock<IRateLimitedRequester>();
+            _matchEndpoint = new MatchEndpoint(_rateLimitedRequester.Object, new PassThroughCache());
+        }
+
+        [TestMethod]
+        public void GetMatchListAsync_Test()
+        {
+            _rateLimitedRequester.Setup(moq => moq.CreateGetRequestAsync(It.IsAny<string>(), It.IsAny<Region>(),
+                It.IsAny<List<string>>(), It.IsAny<bool>())).ReturnsAsync(File.ReadAllText(ResponsePath));
+
+            var matchList = _matchEndpoint.GetMatchListAsync(Region.Euw, "SummonerId").Result;
+
+            Assert.IsNotNull(matchList);
+            foreach(var matchReference in matchList.Matches)
+            {
+                Assert.AreEqual(matchReference.PlatformId, Platform.EUW1);
+                Assert.AreEqual(matchReference.Region, Region.Euw);
+            }
+        }
+        
+    }
+}
