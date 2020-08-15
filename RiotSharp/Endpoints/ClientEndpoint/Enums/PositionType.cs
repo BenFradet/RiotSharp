b86b91884ed5f312b89bb19ab77ee34a233bf91c
Johannes Schneider <johannes-schneider_1995@web.de>
2020-08-16 00:06:27 +0200

    add enum member annotations for PositionType

--- RiotSharp/Endpoints/ClientEndpoint/Enums/PositionType.cs
+++ RiotSharp/Endpoints/ClientEndpoint/Enums/PositionType.cs
@@ -1,11 +1,25 @@
-﻿namespace RiotSharp.Endpoints.ClientEndpoint.Enums
+﻿using System.Runtime.Serialization;
+
+namespace RiotSharp.Endpoints.ClientEndpoint.Enums
 {
     public enum PositionType
     {
+        [EnumMember(Value = "")]
+        Unknown,
+        
+        [EnumMember(Value = "TOP")]
         Top,
+        
+        [EnumMember(Value = "MIDDLE")]
         Middle,
+        
+        [EnumMember(Value = "BOTTOM")]
         Bottom,
+        
+        [EnumMember(Value = "SUPPORT")]
         Support,
+        
+        [EnumMember(Value = "JUNGLE")]
         Jungle,
     }
 }
