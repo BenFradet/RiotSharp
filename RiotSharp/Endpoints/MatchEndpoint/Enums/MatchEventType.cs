80fe0c7fdb08230d1ae5f51c7f8e05db8f5f1169
Tom Eckstein <tom.eckstein@web.de>
2021-08-24 14:24:00 +0200

    Update Match-V5

--- RiotSharp/Endpoints/MatchEndpoint/Enums/MatchEventType.cs
+++ RiotSharp/Endpoints/MatchEndpoint/Enums/MatchEventType.cs
@@ -30,9 +30,29 @@ namespace RiotSharp.Endpoints.MatchEndpoint.Enums
         ChampionKill,
 
         /// <summary>
+        /// Triggers on special champion kills.
+        /// </summary>
+        ChampionSpecialKill,
+
+        /// <summary>
+        /// Triggers on champion transformations.
+        /// </summary>
+        ChampionTransform,
+
+        /// <summary>
+        /// Triggers on dragon souls given to a team.
+        /// </summary>
+        DragonSoulGiven,
+
+        /// <summary>
         /// Triggers on Nashor or Dragon kills.
         /// </summary>
         EliteMonsterKill,
+
+        /// <summary>
+        /// Triggers on end of game.
+        /// </summary>
+        GameEnd,
 
         /// <summary>
         /// Triggers on item destructions.
@@ -55,19 +75,29 @@ namespace RiotSharp.Endpoints.MatchEndpoint.Enums
         ItemUndo,
 
         /// <summary>
-        /// Triggers on champion level up.
+        /// Triggers on champion level ups.
         /// </summary>
         LevelUp,
 
         /// <summary>
-        /// Triggers on start of the game
+        /// Triggers on end of a pause.
         /// </summary>
         PauseEnd,
+
+        /// <summary>
+        /// Triggers on start of a pause.
+        /// </summary>
+        PauseStart,
 
         /// <summary>
         /// Triggers on skill level ups.
         /// </summary>
         SkillLevelUp,
+
+        /// <summary>
+        /// Triggers on destroyed turret plates.
+        /// </summary>
+        TurretPlateDestroyed,
 
         /// <summary>
         /// Triggers on ward kills.
@@ -94,8 +124,16 @@ namespace RiotSharp.Endpoints.MatchEndpoint.Enums
                     return "CAPTURE_POINT";
                 case MatchEventType.ChampionKill:
                     return "CHAMPION_KILL";
+                case MatchEventType.ChampionSpecialKill:
+                    return "CHAMPION_SPECIAL_KILL";
+                case MatchEventType.ChampionTransform:
+                    return "CHAMPION_TRANSFORM";
+                case MatchEventType.DragonSoulGiven:
+                    return "DRAGON_SOUL_GIVEN";
                 case MatchEventType.EliteMonsterKill:
                     return "ELITE_MONSTER_KILL";
+                case MatchEventType.GameEnd:
+                    return "GAME_END";
                 case MatchEventType.ItemDestroyed:
                     return "ITEM_DESTROYED";
                 case MatchEventType.ItemPurchased:
@@ -104,10 +142,16 @@ namespace RiotSharp.Endpoints.MatchEndpoint.Enums
                     return "ITEM_SOLD";
                 case MatchEventType.ItemUndo:
                     return "ITEM_UNDO";
+                case MatchEventType.LevelUp:
+                    return "LEVEL_UP";
                 case MatchEventType.PauseEnd:
                     return "PAUSE_END";
+                case MatchEventType.PauseStart:
+                    return "PAUSE_START";
                 case MatchEventType.SkillLevelUp:
                     return "SKILL_LEVEL_UP";
+                case MatchEventType.TurretPlateDestroyed:
+                    return "TURRET_PLATE_DESTROYED";
                 case MatchEventType.WardKill:
                     return "WARD_KILL";
                 case MatchEventType.WardPlaced:
