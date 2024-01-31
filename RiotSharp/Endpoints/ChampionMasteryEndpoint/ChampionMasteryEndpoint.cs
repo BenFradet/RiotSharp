5c9d9e5dbdb9cb5b5915ab86824f23619164a3a7
niding <6966170+Nicolas-Ding@users.noreply.github.com>
2024-01-31 21:44:59 +0100

    Add Champion Masteries By Puuid endpoints

--- RiotSharp/Endpoints/ChampionMasteryEndpoint/ChampionMasteryEndpoint.cs
+++ RiotSharp/Endpoints/ChampionMasteryEndpoint/ChampionMasteryEndpoint.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -16,6 +17,8 @@ namespace RiotSharp.Endpoints.ChampionMasteryEndpoint
         private const string ChampionMasteriesBySummonerUrl = "/champion-masteries/by-summoner/{0}";
         private const string ChampionMasteryBySummonerUrl = "/champion-masteries/by-summoner/{0}/by-champion/{1}";
         private const string ChampionMasteryTotalScoreBySummonerUrl = "/scores/by-summoner/{0}";
+        private const string ChampionMasteryByPuuidUrl = "/champion-masteries/by-puuid/{0}/by-champion/{1}";
+        private const string ChampionMasteriesByPuuidUrl = "/champion-masteries/by-puuid/{0}";
 
         private readonly IRateLimitedRequester _requester;
 
@@ -29,6 +32,7 @@ namespace RiotSharp.Endpoints.ChampionMasteryEndpoint
         }
 
         /// <inheritdoc />
+        [Obsolete("Endpoints by summonerID are deprecated and will be removed in January/2024. Use the equivalent by PUUID.")]
         public async Task<ChampionMastery> GetChampionMasteryAsync(Region region, string summonerId, long championId)
         {
             var requestUrl = string.Format(ChampionMasteryBySummonerUrl, summonerId, championId);
@@ -38,9 +42,28 @@ namespace RiotSharp.Endpoints.ChampionMasteryEndpoint
         }
 
         /// <inheritdoc />
+        public async Task<ChampionMastery> GetChampionMasteryByPuuidAsync(Region region, string puuid, long championId)
+        {
+            var requestUrl = string.Format(ChampionMasteryByPuuidUrl, puuid, championId);
+
+            var json = await _requester.CreateGetRequestAsync(ChampionMasteryRootUrl + requestUrl, region).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<ChampionMastery>(json);
+        }
+
+        /// <inheritdoc />
+        [Obsolete("Endpoints by summonerID are deprecated and will be removed in January/2024. Use the equivalent by PUUID.")]
         public async Task<List<ChampionMastery>> GetChampionMasteriesAsync(Region region, string summonerId)
         {
             var requestUrl = string.Format(ChampionMasteriesBySummonerUrl, summonerId);
+
+            var json = await _requester.CreateGetRequestAsync(ChampionMasteryRootUrl + requestUrl, region).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<List<ChampionMastery>>(json);
+        }
+
+        /// <inheritdoc />
+        public async Task<List<ChampionMastery>> GetChampionMasteriesByPuuidAsync(Region region, string puuid)
+        {
+            var requestUrl = string.Format(ChampionMasteriesByPuuidUrl, puuid);
 
             var json = await _requester.CreateGetRequestAsync(ChampionMasteryRootUrl + requestUrl, region).ConfigureAwait(false);
             return JsonConvert.DeserializeObject<List<ChampionMastery>>(json);
