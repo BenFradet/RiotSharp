8942fd84770ee984dd0ab9c50de5718161f0678c
asbjornvad <asbjornvad@gmail.com>
2026-01-20 21:26:48 +0100

    Removed old unused stuff.

--- src/RiotSharpNET8/Endpoints/StaticDataEndpoint/ReforgedRune/ReforgedRuneSlotStatic.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RiotSharpNET8.Endpoints.StaticDataEndpoint.ReforgedRune
-{
-    public class ReforgedRuneSlotStatic
-    {
-        public List<ReforgedRuneStatic> Runes { get; set; }
-    }
-}
